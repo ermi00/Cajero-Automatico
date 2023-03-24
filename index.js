950930f8c804f73ce4375a6207b6974dfdb2624b
ermi00 <miguelgonzalez.280803@gmail.com>
2023-03-24 15:00:42 -0600

    Inicio de proyecto

---
+++ index.js
@@ -0,0 +1,10 @@
+// let peliculas = [
+//     {titulo: "DJango", director: "Quentin Tarantino", año: 2016},
+//     {titulo: "Oppenheimer", director: "Christopher Nolan", año: 2024},
+//     {titulo: "Avatar", director: "James Cameron", año: 2010}
+// ]
+
+
+// for(let i = 0; i < peliculas.length; i++){
+//     console.log(peliculas[i].titulo, peliculas[i].director);
+// }
